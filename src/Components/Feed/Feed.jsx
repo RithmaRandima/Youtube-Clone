a67cb6f1d74f279ee1ab184a3d1b35513efa7303
RithmaRandima <rithma.r.nandasiri@gmail.com>
2026-01-23 22:41:20 +0530

    Completed Feed Page

---
+++ src/Components/Feed/Feed.jsx
@@ -0,0 +1,138 @@
+import React from "react";
+import "./Feed.css";
+import pic_1 from "../../assets/pic_1.jpg";
+
+const Feed = () => {
+  return (
+    <div className="feed">
+      {/* card */}
+      <div className="card">
+        <img src={pic_1} alt="" />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>Greetstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+      {/* card */}
+      <div className="card">
+        <img src={pic_1} alt="" />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>Greetstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+      {/* card */}
+      <div className="card">
+        <img src={pic_1} alt="" />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>Greetstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+      {/* card */}
+      <div className="card">
+        <img src={pic_1} alt="" />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>Greetstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+      {/* card */}
+      <div className="card">
+        <img src={pic_1} alt="" />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>Greetstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+      {/* card */}
+      <div className="card">
+        <img src={pic_1} alt="" />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>Greetstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+      {/* card */}
+      <div className="card">
+        <img src={pic_1} alt="" />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>Greetstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+      {/* card */}
+      <div className="card">
+        <img src={pic_1} alt="" />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>Greetstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+      {/* card */}
+      <div className="card">
+        <img src={pic_1} alt="" />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>Greetstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>{" "}
+      {/* card */}
+      <div className="card">
+        <img src={pic_1} alt="" />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>Greetstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>{" "}
+      {/* card */}
+      <div className="card">
+        <img src={pic_1} alt="" />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>Greetstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>{" "}
+      {/* card */}
+      <div className="card">
+        <img src={pic_1} alt="" />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>Greetstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>{" "}
+      {/* card */}
+      <div className="card">
+        <img src={pic_1} alt="" />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>Greetstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>{" "}
+      {/* card */}
+      <div className="card">
+        <img src={pic_1} alt="" />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>Greetstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+    </div>
+  );
+};
+
+export default Feed;
